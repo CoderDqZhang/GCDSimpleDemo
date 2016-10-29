27e9f01a6009b3e0a40c763fefa45f7d0c0d2c67
CoderDqZhang <769839948@qq.com>
2016-10-29 10:58:36 +0800

    第一次上传

---
+++ GCDSimpleDemo/GCDSimpleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDSimpleDemo
+//
+//  Created by Zhang on 29/10/2016.
+//  Copyright © 2016 Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCDSimpleDemo/GCDSimpleDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GCDSimpleDemo
+//
+//  Created by Zhang on 29/10/2016.
+//  Copyright © 2016 Zhang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
